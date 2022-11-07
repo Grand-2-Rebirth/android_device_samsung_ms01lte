71b0f060716091ad8bc251383a08d9a9a2fc9818
Moaz Talaat <88838071+MoazSalem@users.noreply.github.com>
2022-11-07 17:31:51 +0200

    Fix Build

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -13,6 +13,11 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+#Fix Build
+BUILD_BROKEN_DUP_RULES := true
+BUILD_BROKEN_USES_BUILD_COPY_HEADERS := true
+BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
+
 # inherit from common ms01
 include device/samsung/ms01-common/BoardConfigCommon.mk
 
