4af9228aa6bdacba72aa221ae86020c0004ba28c
Moaz Talaat <88838071+MoazSalem@users.noreply.github.com>
2025-05-08 15:30:19 +0300

    Re-Add NFC

--- device.mk
+++ device.mk
@@ -30,5 +30,27 @@ $(call inherit-product, device/samsung/ms01-common/radio/single/board.mk)
 PRODUCT_PACKAGES += \
     Recorder
 
+# NFC packages
+PRODUCT_PACKAGES += \
+   android.hardware.nfc@1.0-impl \
+   com.android.nfc_extras \
+   libnfc_nci \
+   libnfc_nci_jni \
+   NfcNci \
+   Tag
+
+# NFC configs
+PRODUCT_COPY_FILES += \
+	$(LOCAL_PATH)/configs/nfc/nfcee_access.xml:system/etc/nfcee_access.xml \
+	$(LOCAL_PATH)/configs/nfc/libnfc-sec-hal.conf:system/etc/libnfc-sec-hal.conf \
+	$(LOCAL_PATH)/configs/nfc/libnfc-sec.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nci.conf
+
+# NFC Permissions
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.nfc.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.xml \
+	frameworks/native/data/etc/android.hardware.nfc.hce.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.hce.xml \
+	frameworks/native/data/etc/android.hardware.nfc.hcef.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.hcef.xml \
+	frameworks/native/data/etc/com.android.nfc_extras.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.android.nfc_extras.xml
+
 # common ms01
 $(call inherit-product, device/samsung/ms01-common/ms01.mk)
